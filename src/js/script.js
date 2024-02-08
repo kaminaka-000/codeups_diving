7cc7cad013d44313448b7ce8f8e8c878cd32e605
kaminaka <buroguyong64@gmail.com>
2024-02-08 16:21:28 +0900

    2/8 header1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,39 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+        // ハンバーガーメニュー
+        $(function () {
+          $(".js-hamburger").on("click", function () {
+              $(this).toggleClass("is-open");
+              if ($(this).hasClass("is-open")) {
+                  openDrawer();
+              } else {
+                  closeDrawer();
+              }
+          });
+
+          // backgroundまたはページ内リンクをクリックで閉じる
+          $(".js-drawer a[href]").on("click", function () {
+              closeDrawer();
+          });
+
+          // resizeイベント
+          $(window).on('resize', function() {
+              if (window.matchMedia("(min-width: 768px)").matches) {
+                  closeDrawer();
+              }
+          });
+      });
+
+      function openDrawer() {
+          $(".js-sp-nav").fadeIn();
+          $(".js-hamburger").addClass("is-open");
+      }
+
+      function closeDrawer() {
+              $(".js-sp-nav").fadeOut();
+          $(".js-hamburger").removeClass("is-open");
+      }
 
 
 });
