e5f7cb3dddbfd8d2d4e76252c9147dd56637b2da
kaminaka <buroguyong64@gmail.com>
2024-02-11 10:09:45 +0900

    2/11 animation1

--- src/js/script.js
+++ src/js/script.js
@@ -35,7 +35,7 @@ jQuery(function ($) {
             $(".js-hamburger").removeClass("is-open");
         }
 
-    //スライダー
+    //mvスライダー
     var swiper = new Swiper(".js-mv-swiper", {
         loop: true,
         speed: 3000,
@@ -45,11 +45,7 @@ jQuery(function ($) {
         },
     });
 
-    //
-
-
-
-
+    //campaignスライダー
     var swiper = new Swiper(".js-info-card", {
         loop: true,
         slidesPerView: 1.2,
@@ -65,6 +61,35 @@ jQuery(function ($) {
             prevEl: ".swiper-button-prev",
         },
     });
+
+
+    //要素の取得とスピードの設定
+    var box = $('.course__item-img, .testimonial__item-box-img, .price__img-sp, .price__img-pc'),
+    speed = 700;
+
+    //.colorboxの付いた全ての要素に対して下記の処理を行う
+    box.each(function(){
+    $(this).append('<div class="color"></div>')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+            $(this).delay(200).animate({'width':'100%'},speed,function(){
+                image.css('opacity','1');
+                $(this).css({'left':'0' , 'right':'auto'});
+                $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+            }
+        });
+    });
+
+
 
 
 });
